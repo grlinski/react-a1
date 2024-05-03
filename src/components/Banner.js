b444743430ffc5351e7374408df4ee58b268d890
gl-school <gabriellinski@student.mitt.ca>
2024-05-03 10:17:44 -0500

    new deploy

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,14 +3,13 @@
 export default function Banner(props) {
 
   return(
-
     <section className="hero">
       <div className={props.herobg}>
         <h1 className={props.heroheader}>{props.title}</h1>
         <p className={props.herotext}>{props.text}</p>
         <div className="hero-button">
-          <button className={props.primarybtn}>Primary</button>
-          <button className={props.secondarybtn}>Secondary</button>
+          <button className={props.primarybtn}>Synergy</button>
+          <button className={props.secondarybtn}>Paradigm</button>
         </div>
       </div>
     </section>
